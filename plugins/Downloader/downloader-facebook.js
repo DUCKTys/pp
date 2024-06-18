34ddc1f6996dcd17920da10840c43c80fec0c036
Tanaka <projectthenext1532@gmail.com>
2024-06-18 11:27:19 +0700

    Update downloader-facebook.js

--- plugins/Downloader/downloader-facebook.js
+++ plugins/Downloader/downloader-facebook.js
@@ -1,26 +1,3 @@
-/*
-import fetch from 'node-fetch'
-
-let handler = async (m, { conn, command, args, usedPrefix }) => {
-  const linknya = args[0]
-
-  if (!args[0]) throw `Input *URL*`
-  m.reply(wait)
-  try {
-    let res = await fetch(`https://api.ryzendesu.com/api/dowloader/fbdown?url=${encodeURIComponent(linknya)}&apikey=${global.ryzen}`)
-    let result = await res.json()
-    let video = result.result.HD
-    //let audio = result.data.audio
-    let cap = global.wm
-    conn.sendMessage(m.chat, { video: { url: video }, caption: cap }, m)
-    //conn.sendMessage(m.chat, { audio: { url: audio }, mimetype: 'audio/mp4' }, { quoted : m })
-  } catch (e) {
-    console.log(e)
-    m.reply(`Fitur error atau Otak pengguna error`)
-  }
-}
-*/
-
 import axios from 'axios'
 import fetch from 'node-fetch'
 
